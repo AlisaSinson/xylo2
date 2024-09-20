3378de47ab6804827e404ee7b20253fdc0ff8169
Alisa <alisa.bajenova@gmail.com>
2024-09-20 16:30:38 +0300

    complete

--- xylophone/ViewController.swift
+++ xylophone/ViewController.swift
@@ -4,16 +4,26 @@
 //
 //  Created by Алиса on 17.09.2024.
 //
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        playSound(title: sender.titleLabel?.text)
+    }
+    
+    func playSound(title: String?) {
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
